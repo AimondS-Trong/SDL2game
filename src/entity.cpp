c0f035c32b9327866612d37b353ea3f3ba500145
AimondS-Trong <24020151@vnu.edu.vn>
2025-03-31 11:01:31 +0700

    update comment for entity.cpp

--- src/entity.cpp
+++ src/entity.cpp
@@ -2,7 +2,7 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
-Entity::Entity(Vector2f p_pos, SDL_Texture* p_tex)
+Entity::Entity(Vector2f p_pos, SDL_Texture* p_tex) // Khởi tạo lớp entity
 :pos(p_pos), tex(p_tex)
 {
 	currentFrame.x = 0;
@@ -12,33 +12,39 @@ Entity::Entity(Vector2f p_pos, SDL_Texture* p_tex)
 	SDL_QueryTexture(p_tex, NULL, NULL, &currentFrame.w, &currentFrame.h);
 }
 
+// Khai báo texture theo entity
 SDL_Texture* Entity::getTex()
 {
 	return tex;
 }
 
+// Hàm cập nhật (cho các lớp kế thừa)
 void Entity::update(double deltaTime)
 {
 
 }
 
+// Hàm thiết lập vị trí (cho các lớp kế thừa)
 void Entity::setPos(float x, float y)
 {
 	pos.x = x;
 	pos.y = y;
 }
 
+// Hàm thiết lặp góc quay (Cho ball)
 void Entity::setAngle(float p_angle)
 {
 	angle = p_angle;
 }
 
+// Hàm thiết lập tỉ lệ (cho các lớp kế thừa)
 void Entity::setScale(float w, float h)
 {
 	scale.x = w;
 	scale.y = h;
 }
 
+// Hàm lấy kích thước vị trí texture
 SDL_Rect Entity::getCurrentFrame()
 {
 	return currentFrame;
