b8fabe48572f9845d0ce88e0fc7ec17c9d4bf333
AimondS-Trong <24020151@vnu.edu.vn>
2025-03-31 10:57:51 +0700

    update comment for hole.cpp

--- src/hole.cpp
+++ src/hole.cpp
@@ -3,7 +3,7 @@
 #include "Math.h"
 #include <SDL2/SDL.h>
 
-// Kế thừa class của Entity và lưu vị trí và hình ảnh của hole
+// Kế thừa lớp Entity và lưu vị trí và hình ảnh của hole
 Hole::Hole(Vector2f p_pos, SDL_Texture* p_tex)
 :Entity(p_pos, p_tex)
 {
